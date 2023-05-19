e0eb14ec504566b933f1d6f4cf8bb290b25bd3d7
Tolkinxon <krosstony639@gmail.com>
2023-05-19 12:11:50 +0500

    some changes

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -13,12 +13,13 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <div className="container">
+      <div
+        className={`container-fluid border border-3 border-dark hj${indexSass.Container}`}
+      >
         <div className="row">
-          <div className="col-3"></div>
-          <div className="col-6"></div> 
-          <div className="col-3"></div>
+          <div className="col-3 bg-success ">a</div>
+          <div className="col-6 bg-warning ">s</div>
+          <div className="col-3 bg-secondary  ">d</div>
         </div>
       </div>
 
