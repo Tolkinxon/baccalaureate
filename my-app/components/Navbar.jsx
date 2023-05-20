a66d9d4fde025483e7d5aa9be89f670dfac19f34
Tolkinxon <krosstony639@gmail.com>
2023-05-20 16:30:31 +0500

    added some pictures

--- my-app/components/Navbar.jsx
+++ my-app/components/Navbar.jsx
@@ -5,14 +5,19 @@ import Image from 'next/image'
 const Navbar = () => {
   return (
     <div className={styles.navbar}>
-      <div>
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-mortarboard-fill" viewBox="0 0 16 16">
+    
+      <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" className={`${styles.svg}`} viewBox="0 0 16 16">
         <path d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3a.5.5 0 0 0 .372 0L14 7.14V13a1 1 0 0 0-1 1v2h3v-2a1 1 0 0 0-1-1V6.739l.686-.275a.5.5 0 0 0 .025-.917l-7.5-3.5Z"/>
         <path d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .294.605l4.5 1.8a.5.5 0 0 0 .372 0l4.5-1.8a.5.5 0 0 0 .294-.605l-.5-1.7a.5.5 0 0 0-.656-.327L8 10.466 4.176 9.032Z"/>
       </svg>
+      
+      <div>Tashkent State Government Univercity named I.A.Karimov </div>
+
+      <div>
+          <Image  src='/4x4picture.jpg' width={50} height={50} className={`${styles.img}`}/>
+          <div>John Taylor</div>
       </div>
-      <p>Name univercity</p>
-      <div>identitu teacher</div>
+
     </div>
   )
 }
