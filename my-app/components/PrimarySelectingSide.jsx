f985d21e150c07e3bcdfb5818a87a642d494e1e1
Tolkinxon <krosstony639@gmail.com>
2023-05-20 19:24:32 +0500

    focus fixed

--- my-app/components/PrimarySelectingSide.jsx
+++ my-app/components/PrimarySelectingSide.jsx
@@ -4,11 +4,16 @@ import { useState } from 'react'
 
 const PrimarySelectingSide = () => {
   const [toggle, setToggle] = useState(false)
+  const [focus, setFocus] = useState(1)
 
   return (
     <>
       <div>
-        <div className={classes.first_line}>
+        <div
+          className={classes.first_line}
+          style={{ color: focus === 1 ? 'burlywood' : 'white' }}
+          onClick={() => setFocus(1)}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="16"
@@ -20,6 +25,7 @@ const PrimarySelectingSide = () => {
             <path d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5L14 4.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h-2z" />
             <path d="M8.646 6.646a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1 0 .708l-2 2a.5.5 0 0 1-.708-.708L10.293 9 8.646 7.354a.5.5 0 0 1 0-.708zm-1.292 0a.5.5 0 0 0-.708 0l-2 2a.5.5 0 0 0 0 .708l2 2a.5.5 0 0 0 .708-.708L5.707 9l1.647-1.646a.5.5 0 0 0 0-.708z" />
           </svg>
+
           <span>Ilmiy ishlar</span>
         </div>
 
@@ -69,8 +75,18 @@ const PrimarySelectingSide = () => {
           </button>
 
           <ul>
-            <li>Dars mavzulari</li>
-            <li>Davomat</li>
+            <li
+              style={{ color: focus === 2 ? 'burlywood' : 'white' }}
+              onClick={() => setFocus(2)}
+            >
+              Dars mavzulari
+            </li>
+            <li
+              style={{ color: focus === 3 ? 'burlywood' : 'white' }}
+              onClick={() => setFocus(3)}
+            >
+              Davomat
+            </li>
           </ul>
         </div>
       </div>
