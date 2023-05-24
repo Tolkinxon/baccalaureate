4b5b3cd67a50875bbd08817654ad8ff8c1ec8a30
Tolkinxon <krosstony639@gmail.com>
2023-05-24 05:28:55 +0500

    some changes

--- my-app/services/index.js
+++ my-app/services/index.js
@@ -1,30 +1,5 @@
 import { gql, request } from 'graphql-request'
 
-// const takingData = async () => {
-//   const key =
-//     'https://api-us-east-1-shared-usea1-02.hygraph.com/v2/clhzk311p2tj701upbgne20vs/master'
-//   const query = gql`
-//     query Assets {
-//       ratingAttendancesConnection {
-//         edges {
-//           node {
-//             attendance
-//             date
-//             group
-//             name
-//             score
-//             theme
-//             id
-//           }
-//         }
-//       }
-//     }
-//   `
-
-//   const result = await request(key, query)
-//   return result.ratingAttendancesConnection.edges
-// }
-// export default takingData
 
 const takingData = async () => {
   const key =
