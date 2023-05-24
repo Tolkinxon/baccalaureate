fe5ecfcb4b4edebc16f7eff1b8cdac43f9b4eb92
Tolkinxon <krosstony639@gmail.com>
2023-05-24 13:10:44 +0500

    scientific works side news

--- my-app/components/ScientificWorks.jsx
+++ my-app/components/ScientificWorks.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import styles from '../styles/groups/groupsThemes.module.scss'
 
 const ScientificWorks = () => {
-  return <div className={`border border-dark ${styles.groups}`}>helo</div>
+  return (
+    <div className={` shadow ${styles.groups} ${styles.groups2}`}>helo</div>
+  )
 }
 
 export default ScientificWorks
