0f9205a8f0bf4c71857d7a0854b5a9850cf331ea
Tolkinxon <krosstony639@gmail.com>
2023-05-24 05:15:52 +0500

    some changes

--- my-app/services/index.js
+++ my-app/services/index.js
@@ -24,5 +24,25 @@ const takingData = async () => {
   const result = await request(key, query)
   return result.ratingAttendancesConnection.edges
 }
-
 export default takingData
+
+const takingData2 = async () => {
+  const key =
+    'https://api-us-east-1-shared-usea1-02.hygraph.com/v2/clhzk311p2tj701upbgne20vs/master'
+
+  const query = gql`
+    query Assets {
+      tutorialsConnection {
+        edges {
+          node {
+            tutorial1
+          }
+        }
+      }
+    }
+  `
+
+  const result = await request(key, query)
+  return result.tutorialsConnection.edges[0].node.tutorial1[0].edges
+}
+export { takingData2 }
