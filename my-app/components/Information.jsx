5281e88267d2275b4d4dd62b31b2cfc5f249ea17
Tolkinxon <krosstony639@gmail.com>
2023-05-24 11:24:43 +0500

    some changes

--- my-app/components/Information.jsx
+++ my-app/components/Information.jsx
@@ -4,29 +4,19 @@ import styles from '../styles/information/information.module.scss'
 const Information = () => {
   return (
     <div className={`shadow  ${styles.information}`}>
-      <p className="display-6">Ma'lumot</p>
-      <table class="table table-borderless">
-        <tbody>
-          <tr className="row">
-            <th scope="row" className="col">
-              fanlar
-            </th>
-            <td className="col-8">Mark</td>
-          </tr>
-          <tr className="row">
-            <th scope="row" className="col">
-              Mashg'ulot
-            </th>
-            <td className="col-8">Jacob</td>
-          </tr>
-          <tr className="row">
-            <th scope="row" className="col">
-              Dars sanalari
-            </th>
-            <td className="col-8">Jacob</td>
-          </tr>
-        </tbody>
-      </table>
+      <p className="">Total informations</p>
+      <div>
+        <span className='fw-bold'>Amount subjects</span>
+        <span>2</span>
+      </div>
+      <div>
+        <span className='fw-bold'>Amount Groups</span>
+        <span>3</span>
+      </div>
+      <div>
+        <span className='fw-bold'>Dates of subjects</span>
+        <span>3</span>
+      </div>
     </div>
   )
 }
